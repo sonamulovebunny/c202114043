e33c5f877866a1b9d359cacb58f7c9788ee25190
sonamu <skit21sol@gmal.com>
2025-04-09 09:41:07 +0900

    three

--- src/c202114043/Main.java
+++ src/c202114043/Main.java
@@ -2,5 +2,7 @@ package c202114043;
 
 public class Main {
 	public static void main(String[] args) { 
-		System.out.println("임예솔"); }
+		System.out.println("임예솔"); 
+		World.run();
+	}
 }
---
+++ src/c202114043/World.java
@@ -0,0 +1,6 @@
+package c202114043;
+
+public class World { 
+    public static void run() { 
+        System.out.println("202114043"); } 
+    }
